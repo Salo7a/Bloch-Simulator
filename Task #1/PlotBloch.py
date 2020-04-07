8c2e2b9f7d16b5c918ffab3bfcb9aba676eab81e
Salo7a <salo7a98x@gmail.com>
2020-04-07 11:10:58 +0200

    Added Excitation To Plot Bloch

--- Task #1/PlotBloch.py
+++ Task #1/PlotBloch.py
@@ -14,12 +14,12 @@ sphere = Bloch(axes=ax)
 sphere.zlabel = ['z', '-z']
 
 # init the data of the MRI.
-t = 1
+t = 0
 M0 = 1  # mag of M0
 T1 = 6  # T1 Value
 T2 = 3  # T2 Value
 
-Mxy = M0 * np.exp(-t / T2)
+Mxy = M0
 # Mx = M0 * np.exp(-t/T2) * np.sin(2*np.pi*1*t)
 # My = M0 * np.exp(-t/T2) * np.cos(2*np.pi*1*t)
 Mz = M0 * (1 - np.exp(-t / T1))
@@ -29,7 +29,7 @@ M = [0, 0, 1]
 sphere.add_vectors(M)
 rotated = False
 i = 0
-
+elapsed = 0
 
 def yrot(vector, theta):
     vec = vector
@@ -40,31 +40,42 @@ def yrot(vector, theta):
 
     rotation_vector = rotation_radians * rotation_axis
     rotation = R.from_rotvec(rotation_vector)
-    return rotation.apply(vec)
+
+    sec = rotation.apply(vec)
+    rotation_degrees = theta / 2
+    rotation_radians = np.radians(rotation_degrees)
+    rotation_axis = np.array([0, 0, 1])
+
+    rotation_vector = rotation_radians * rotation_axis
+    rotation = R.from_rotvec(rotation_vector)
+    return rotation.apply(sec)
 
 
 def updateAnimation(t):
-    global Mxy, Mz, Mx, My, M, i
+    global Mxy, Mz, Mx, My, M, i, rotated, elapsed
     sphere.clear()
     if i < 90:
         r = yrot(M, 1)
         M = r
         i += 1
-        t = 1
     else:
-        Mxy = M0 * np.exp(-t / T2)
+        if not rotated:
+            rotated = True
+            elapsed = t
+        Mxy = M0 * np.exp(-(t - elapsed) / T2)
         # Mx = M0 * np.exp(-t / T2) * np.sin(2 * np.pi * 1 * t)
         # My = M0 * np.exp(-t / T2) * np.cos(2 * np.pi * 1 * t)
-        Mz = M0 * (1 - np.exp(-t / T1))
+        Mz = M0 * (1 - np.exp(-(t - elapsed) / T1))
         M[0] = Mxy
         M[1] = Mxy
         # M[0] = Mx
         # M[1] = My
         M[2] = Mz
+        print(M)
     sphere.add_vectors(M)
     sphere.make_sphere()
 
 
 ani = FuncAnimation(figure, updateAnimation, frames=np.arange(1, 40, 0.1), interval=50)
-ani.save("RelaxationWithPrecession.mp4")
+# ani.save("RelaxationWithPrecession.mp4")
 plt.show()
